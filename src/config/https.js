59c0dd2c8bb9fa2d067baa2148d99679ddf35880
M Hasan <hasanmuhammad197@gmail.com>
2023-06-23 16:34:19 +0700

    back api https

--- src/config/https.js
+++ src/config/https.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://34.101.78.228:2424/api/v1'
+  baseURL: 'https://be-golang.kucinghitam.tech/api/v1'
 });
 
 api.interceptors.request.use(
